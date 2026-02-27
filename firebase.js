8ea34602ebedf6e73b7a1d3b3ea3d03b87774506
RAJU PATIL <patil.raju.1015@gmail.com>
2026-02-27 16:08:09 +0530

    Update firebase.js
    the firebase is set up with all updates

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
-// firebase.js
-
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
+// Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAn4QIOCSnCF_-Tm27PTpO_FVhrFOTasio",
-    authDomain: "smarteduid-caeeb.firebaseapp.com",
-    projectId: "smarteduid-caeeb",
-    storageBucket: "smarteduid-caeeb.firebasestorage.app",
-    messagingSenderId: "39611174945",
-    appId: "1:39611174945:web:a601c455d02d54e0ca1360"
+    apiKey: "AIzaSyCkfmna16tf-rahGWdP7SQ3LPDjKU_Vayw",
+    authDomain: "smarteduid-2ebe6.firebaseapp.com",
+    projectId: "smarteduid-2ebe6",
+    storageBucket: "smarteduid-2ebe6.firebasestorage.app",
+    messagingSenderId: "390173400544",
+    appId: "1:390173400544:web:aa7add5e60a4e2d7181f85"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
